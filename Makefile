63ebe3e9e8d5f1fbf5e2ce59beebc14d18c63072
moustachu <alain@opensourcepolitics.eu>
2024-02-28 12:06:10 +0100

    Initial commit

---
+++ Makefile
@@ -0,0 +1,39 @@
+run: up
+	@make create-seeds
+
+up:
+	docker-compose -f docker-compose.local.yml up --build -d
+	@make setup-database
+
+# Stops containers and remove volumes
+teardown:
+	docker-compose -f docker-compose.local.yml down -v --rmi all
+
+create-database:
+	docker-compose -f docker-compose.local.yml exec app /bin/bash -c 'DISABLE_DATABASE_ENVIRONMENT_CHECK=1 /usr/local/bundle/bin/bundle exec rake db:create'
+
+setup-database: create-database
+	docker-compose -f docker-compose.local.yml exec app /bin/bash -c 'DISABLE_DATABASE_ENVIRONMENT_CHECK=1 /usr/local/bundle/bin/bundle exec rake db:migrate'
+
+# Create seeds
+create-seeds:
+	docker-compose -f docker-compose.local.yml exec app /bin/bash -c 'DISABLE_DATABASE_ENVIRONMENT_CHECK=1 /usr/local/bundle/bin/bundle exec rake db:schema:load db:seed'
+
+# Restore dump
+restore-dump:
+	bundle exec rake restore_dump
+
+shell:
+	docker-compose -f docker-compose.local.yml exec app /bin/bash
+
+restart:
+	docker-compose -f docker-compose.local.yml up -d
+
+status:
+	docker-compose -f docker-compose.local.yml ps
+
+logs:
+	docker-compose -f docker-compose.local.yml logs app
+
+rebuild:
+	docker-compose -f docker-compose.local.yml up --build -d
---
+++ config/application.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+require_relative "boot"
+
+require "decidim/rails"
+# Add the frameworks used by your app that are not loaded by Decidim.
+require "action_cable/engine"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+
+require "wicked_pdf"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module DevelopmentApp
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+    config.autoloader = :zeitwerk
+    config.time_zone = "Europe/Paris" unless Rails.env.test?
+    config.i18n.load_path += Dir[Rails.root.join("config/locales/**/*.yml").to_s]
+
+    # This needs to be set for correct images URLs in emails
+    # DON'T FORGET to ALSO set this in `config/initializers/carrierwave.rb`
+    config.action_mailer.asset_host = "https://#{Rails.application.secrets[:asset_host]}/" if Rails.application.secrets[:asset_host].present?
+    config.backup = config_for(:backup).deep_symbolize_keys
+
+    config.action_dispatch.default_headers = {
+      "X-Frame-Options" => "SAMEORIGIN",
+      "X-XSS-Protection" => "1; mode=block",
+      "X-Content-Type-Options" => "nosniff"
+    }
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+
+    config.to_prepare do
+      require "extends/helpers/decidim/forms/application_helper_extends"
+      require "extends/cells/decidim/forms/step_navigation_cell_extends"
+    end
+
+    config.after_initialize do
+      require "extends/controllers/decidim/devise/sessions_controller_extends"
+      require "extends/controllers/decidim/editor_images_controller_extends"
+      require "extends/services/decidim/iframe_disabler_extends"
+      require "extends/helpers/decidim/icon_helper_extends"
+      require "extends/commands/decidim/initiatives/admin/update_initiative_answer_extends"
+      require "extends/controllers/decidim/initiatives/committee_requests_controller_extends"
+      require "extends/models/decidim/budgets/project_extend"
+
+      Decidim::GraphiQL::Rails.config.tap do |config|
+        config.initial_query = "{\n  deployment {\n    version\n    branch\n    remote\n    upToDate\n    currentCommit\n    latestCommit\n    locallyModified\n  }\n}".html_safe
+      end
+    end
+
+    if ENV.fetch("RAILS_SESSION_STORE", "") == "active_record"
+      initializer "session cookie domain", after: "Expire sessions" do
+        Rails.application.config.session_store :active_record_store, key: "_decidim_session", expire_after: Decidim.config.expire_session_after
+        ActiveRecord::SessionStore::Session.serializer = :hybrid
+      end
+    end
+  end
+end
